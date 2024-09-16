7972752d5f94b0d0573cfb4fc02cba1074000fe8
itisaarjan <arjansubedi@usf.edu>
2024-09-16 18:43:59 -0400

    Deploying

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,26 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 function HeroSection() {
   return (
-    <div className='bg-white pl-12 pr-12 z-10 h-[30vh] mb-4'>
-        <div className="hero bg-white ">
-  <div className="hero-content flex-col lg:flex-row-reverse pt-32">
-    {/* <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" /> */}
-    <div className='w-[60vw]'>
-      <h1 className="text-5xl font-bold text-black z-0">SimrikMed </h1>
-      <p className="text-black w-[60%] mt-4">
-        {/* Urgent Care and Medical Weight Loss Clinic. */}
-        <h1 className='text-2xl'> Coming soon...</h1>
-      </p>
-      <a className="btn btn-primary bg-red-500 border-none hover:bg-red-500/80 mt-4 text-white" href='https://www.optimantra.com/optimus/om/patient/login' target='_blank'>Make Appointment</a>
-      {/* <a className="btn btn-primary bg-red-500 border-none hover:bg-red-500/80 mt-4 text-white ml-4" href='tel:+18139927839'>Make a call</a> */}
+    <div className='bg-white px-4 sm:px-8 md:px-12 z-10 mb-4'>
+      <div className="hero bg-white">
+        <div className="hero-content flex-col lg:flex-row-reverse pt-16 lg:pt-32">
+          <div className='w-full lg:w-[60vw]'>
+            <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-black">SimrikMed</h1>
+            <p className="text-black mt-4 text-lg sm:text-xl lg:text-2xl">
+              Coming soon...
+            </p>
+            <a className="btn btn-primary bg-red-500 border-none hover:bg-red-500/80 mt-4 text-white" href='https://www.optimantra.com/optimus/om/patient/login' target='_blank'>Make Appointment</a>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- src/Components/ServiceList.jsx
+++ src/Components/ServiceList.jsx
@@ -1,58 +1,24 @@
-import React from 'react'
-import weightLoss from "../assets/weightloss.jpeg"
-import ServiceIcon from './ServiceIcon'
-import primarycare from '../assets/primarcycare.jpg'
-import familyPlannning from '../assets/familyPlanning.jpg'
-import physicalExams from "../assets/PhysicalExams.jpg"
-import chronic from '../assets/chronic.jpg'
-import rapidlab from '../assets/rapidlabtests.webp'
-import vaccination from '../assets/vaccination.jpg'
-import virtualCare from '../assets/virtualcare.png'
-import medicalClearence from '../assets/man-signing-medical-clearance-forms copy.webp'
-import ivFluids from '../assets/iv-fluids.avif'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import ServiceIcon from './ServiceIcon';
+import weightLoss from "../assets/weightloss.jpeg";
+import virtualCare from '../assets/virtualcare.png';
+import { Link } from 'react-router-dom';
 
 function ServiceList() {
   return (
-    <>
-      <div className='mt-16 flex flex-col items-center justify-center gap-[2rem] ml-12 mr-16 h-[60vh] mt-4'>
-        <h1 className='text-4xl text-black'>Services</h1>
-        <hr/>
-        <div className='grid grid-cols-1 gap-[2rem] lg:grid-cols-4'>
-          {/* <Link to='/services/primary-care-services'>
-            <ServiceIcon image={primarycare} title="Primary Care Services"/>
-          </Link> */}
-          <Link to='/services/medical-weight-loss'>
-            <ServiceIcon image={weightLoss} title="Medical Weight Loss"/>
-          </Link>
-          {/* <Link to='/services/woman-wellness-family-planning'>
-            <ServiceIcon image={familyPlannning} title="Woman Wellness & Family Planning"/>
-          </Link> */}
-          {/* <Link to='/services/physical-exams'>
-            <ServiceIcon image={physicalExams} title="Physical Exams"/>
-          </Link> */}
-          {/* <Link to='/services/chronic-medical-conditions'>
-            <ServiceIcon image={chronic} title="Chronic Medical Conditions"/>
-          </Link> */}
-          {/* <Link to='/services/rapid-lab-tests-screening'>
-            <ServiceIcon image={rapidlab} title="Rapid Lab Tests & Screening"/>
-          </Link> */}
-          {/* <Link to='/services/in-house-vaccination'>
-            <ServiceIcon image={vaccination} title="In-House Vaccination"/>
-          </Link> */}
-          <Link to='/services/virtual-care'>
-            <ServiceIcon image={virtualCare} title="Virtual Care"/>
-          </Link>
-          {/* <Link to='/services/medical-clearance'>
-            <ServiceIcon image={medicalClearence} title="Medical Clearance"/>
-          </Link> */}
-          {/* <Link to='/services/iv-fluids'>
-            <ServiceIcon image={ivFluids} title="IV Fluids"/>
-          </Link> */}
-        </div>
+    <div className='mt-8 px-4 sm:px-8 lg:px-12 mb-16'>
+      <h1 className='text-3xl sm:text-4xl lg:text-5xl text-black text-center'>Services</h1>
+      <hr className='my-4'/>
+      <div className='grid grid-cols-1 gap-4 sm:gap-6 lg:gap-8 lg:grid-cols-2 xl:grid-cols-3'>
+        <Link to='/services/medical-weight-loss'>
+          <ServiceIcon image={weightLoss} title="Medical Weight Loss"/>
+        </Link>
+        <Link to='/services/virtual-care'>
+          <ServiceIcon image={virtualCare} title="Virtual Care"/>
+        </Link>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default ServiceList
+export default ServiceList;
